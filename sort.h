f1990efbe94665817d8326ddb5d100a0e7e378c6
D7-57 <10665@holbertonstudents.com>
2025-04-11 18:19:38 +0000

    T

---
+++ sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/* Provided print functions */
+void print_array(const int *array, size_t size);
+
+/* Sorting algorithms */
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
